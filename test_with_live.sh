2d2855b101c63331d4394baf96350843e53f13c2
Magnus Manske <magnusmanske@googlemail.com>
2025-11-13 14:27:39 +0000

    better test

--- test_with_live.sh
+++ test_with_live.sh
@@ -4,17 +4,21 @@ do
   # each line needs to start with "geohack.php?"
 
   url_local="http://localhost:8000/$line"
-  #echo "$url_local"
-
   url_live="https://geohack.toolforge.org/$line"
-  #echo "$url_live"
 
   curl -sg "$url_live" > live.html
   curl -sg "$url_local" > local.html
 
-  live_bytes=$(wc -c < live.html)
-  local_bytes=$(wc -c < local.html)
+  diff=$(diff -b live.html local.html)
+  if [ -z "$diff" ]; then
+    echo "$line : OK"
+  else
+    live_bytes=$(wc -c < live.html)
+    local_bytes=$(wc -c < local.html)
+    echo
+    echo "$line : $live_bytes / $local_bytes bytes"
+    echo "$diff"
+    echo
+  fi
 
-  echo "$line : $live_bytes / $local_bytes"
-  diff -b live.html local.html
 done < test_params.txt
